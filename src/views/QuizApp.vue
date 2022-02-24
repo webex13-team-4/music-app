c8eca3d2836fde5ac169cd994233a72454e4d6d2
nonononoka <onon.kaori@gmail.com>
2022-02-24 18:22:13 +0900

    クイズアプリ作った

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,41 +1,49 @@
 <template>
-  <!-- <h1>Vue クイズ</h1>
-  <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
-    <img
-      class="quiz-image"
-      src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
-    />
-    <div class="container">
-      <button>
-        {{ "選択肢1" }}
-      </button>
-      <button>
-        {{ "選択肢2" }}
-      </button>
-      <button>
-        {{ "選択肢3" }}
-      </button>
-    </div>
-    <div>{{ "答え" }}</div>
-  </div> -->
-
-  <div id="app">
-    <h2>Q. {{ quiz.text }}</h2>
-    <img id="quiz-image" v-bind:src="quizImagePath" v-bind:alt="quiz.text" />
-    <div class="container">
-      <button
-        v-for="(choice, i) in quiz.choices"
-        v-bind:key="i"
-        v-on:click="choiced(choice)"
-      >
-        {{ choice.text }}
-      </button>
-    </div>
-    <div>{{ feedback }}</div>
+  <h2 id="title">Q.{{ quiz.text }}</h2>
+  <div class="quizimage">
+    <img v-bind:src="quizImagepath" width="300" height="400" />
   </div>
+  <div class="question">
+    <button
+      v-for="question in quiz.questions"
+      :key="question.id"
+      v-on:click="doBtn(question)"
+      id="question"
+    >
+      {{ question.text }}
+    </button>
+  </div>
+  <div class="feedback">{{ feedback }}</div>
 </template>
+
+<style>
+#title {
+  text-align: center;
+  width: 500px;
+  margin: 0 auto;
+}
+
+.quizimage {
+  margin-top: 40px;
+  text-align: center;
+}
+
+.question {
+  display: flex;
+  height: 80px;
+  width: 700px;
+  padding: 20px;
+  margin: 0 auto;
+  justify-content: space-around;
+}
+
+button#question {
+  margin: 10px;
+}
+.feedback {
+  text-align: center;
+}
+</style>
 
 <script>
 export default {
@@ -43,65 +51,40 @@ export default {
     return {
       feedback: "",
       quiz: {
-        text: "この星の名前は何でしょう？",
-        image: "Ganymede.jpg",
-        choices: [
+        text: "これは、シャーロックホームズです。シャーロックホームズが作中で実際に述べた言葉は次のうちどれでしょう？",
+        image: "detective.png",
+        questions: [
           {
-            text: "ゴリアテ",
-            isCorrect: false,
-            feedback:
-              "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+            id: 0,
+            text: "君を確実に破滅させることができるなら、公共の利益のために僕は喜んで死を受け入れよう",
+            answer:
+              "あたり!1893年「最後の事件」での台詞です!宿敵のモリアーティ教授への言葉ですね。名探偵コナンでも登場しました。",
           },
           {
-            text: "ゼニガメ",
-            isCorrect: false,
-            feedback:
-              "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+            id: 1,
+            text: "想像力はよき下僕だが、主人には不向きだ。もっとも単純な説明がいつもたいてい当たっている。",
+            answer:
+              "ブッブー。これは、エスキュールポワロの台詞です。オリエント急行殺人事件が有名ですね。",
           },
           {
-            text: "ガニメデ",
-            isCorrect: true,
-            feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+            id: 2,
+            text: "思い込みはいつだって敵だ。見えるものも見えなくしてしまう。",
+            answer:
+              "残念!東野圭吾さんの有名作品ガリレオシリーズでの言葉です。容疑者Xの献身はおすすめ！",
           },
         ],
       },
     }
   },
   methods: {
-    choiced(choice) {
-      this.feedback = choice.feedback
-
-      if (choice.isCorrect) {
-        // 次の問題へ
-      }
+    doBtn(question) {
+      this.feedback = question.answer
     },
   },
   computed: {
-    quizImagePath() {
-      return "./images/" + this.quiz.image
+    quizImagepath() {
+      return require("@/assets/" + this.quiz.image)
     },
   },
 }
 </script>
-
-<style>
-#app {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: center;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-#quiz-image {
-  height: 300px;
-  width: 300px;
-  object-fit: contain;
-}
-.container {
-  display: flex;
-  height: 2em;
-  width: 300px;
-  padding: 1em;
-  justify-content: space-around;
-}
-</style>
