fd200bb94921fc4a088428ef04e9f44416ded8de
nonononoka <onon.kaori@gmail.com>
2022-03-02 15:29:38 +0900

    アニメーションつけてみた

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,18 @@
   <h1>Home</h1>
 
   <div class="navigation">
-    <div class="nav-inner">
-      <router-link to="/" class="navigation__link">
-        <div class="img-container">
-          <img src="@/assets/home.jpg" class="img" />
-        </div>
-        <div class="explain-container">Home</div>
-      </router-link>
-    </div>
-
-    <div class="nav-inner">
-      <router-link to="/quiz" class="navigation__link">
-        <div class="img-container">
-          <img src="@/assets/quiz.jpg" class="img" />
-        </div>
-        <div class="explain-container">クイズ</div></router-link
-      >
-    </div>
-
-    <div class="nav-inner">
-      <router-link to="/palette" class="navigation__link">
-        <div class="img-container">
-          <img src="@/assets/palette.jpg" class="img" />
-        </div>
-        <div class="explain-container">パレット</div>
-      </router-link>
-    </div>
-
-    <div class="nav-inner">
-      <router-link to="/memo" class="navigation__link">
-        <div class="img-container">
-          <img src="@/assets/memo.jpg" class="img" />
-        </div>
-        <div class="explain-container">メモ</div>
+    <div
+      class="nav-inner"
+      v-for="content in contents"
+      :key="content.id"
+      v-on:mouseover="mouseOverAction(content)"
+      v-on:mouseleave="mouseLeaveAction(content)"
+    >
+      <router-link :to="`/${content.name}`" class="navigation__link">
+        <img :src="require(`@/assets/${content.name}.jpg`)" class="img" />
+        <transition>
+          <p v-if="content.show">{{ content.title }}</p>
+        </transition>
       </router-link>
     </div>
   </div>
@@ -47,21 +26,74 @@
   justify-content: center;
 }
 
-.explain-container {
-  text-align: center;
-  height: 50px;
-}
-
 .nav-inner {
-  width: 200px;
+  width: 250px;
+  height: 250px;
   border: solid;
   border-radius: 20%;
   overflow: hidden;
   margin: 20px;
+  position: relative;
 }
 
 .img {
-  width: 200px;
-  height: 200px;
+  width: 250px;
+  height: 250px;
+}
+.img:hover {
+  filter: blur(10px);
+  transition: 2s;
+}
+.nav-inner p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 20px;
+  color: white;
+}
+.v-enter-active {
+  transition: opacity 2s;
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      contents: [
+        {
+          id: 0,
+          title: "Quiz",
+          show: false,
+          name: "quiz",
+        },
+        {
+          id: 1,
+          title: "Palette",
+          show: false,
+          name: "palette",
+        },
+        {
+          id: 2,
+          title: "Memo",
+          show: false,
+          name: "memo",
+        },
+      ],
+    }
+  },
+  methods: {
+    mouseOverAction(content) {
+      content.show = true
+    },
+    mouseLeaveAction(content) {
+      content.show = false
+    },
+  },
+}
+</script>
